4f94c62428225c34d61c21a4cc96fa780dd599ca
João Santos <ricardoelric@gmail.com>
2024-02-22 01:46:42 -0300

    (imp): making the platform rotation smooth and returning to the neutral position

---
+++ _Project/Code/Runtime/Common/Math.gd
@@ -0,0 +1,4 @@
+class_name Math
+
+static func MinMax(value : float, min : float, max : float):
+	return minf(max, maxf(min, value))
--- _Project/Code/Runtime/FloorController.gd
+++ _Project/Code/Runtime/FloorController.gd
@@ -2,15 +2,55 @@ class_name FloorController extends MeshInstance3D
 
 var input : Vector3
 
-@export var maxAngle : float = 2.9
+@export var maxAngle : float = 0.2
+@export var angularSpeed : float = 1
 
-func _process(delta):
+var previousXDirection : int = 0
+var previousZDirection : int = 0
+
+var jumpPreviousX : bool = false
+var jumpPreviousZ : bool = false
+
+func _process(delta : float):
 	input = Vector3.ZERO
-	
 	input.x = Input.get_axis("back", "forward")
 	input.z = Input.get_axis("left", "right")
+
+	tryResetRotation()
+	rotation = buildTargetPosition(delta)
+	tryUpdatePreviousDirections()
+
+func tryResetRotation():
+	jumpPreviousX = false
+	jumpPreviousZ = false
+
+	if input.x == 0 and rotation.x != 0:
+		jumpPreviousX = true
+		input.x = -previousXDirection
+		
+	if input.z == 0 and rotation.z != 0:
+		jumpPreviousZ = true
+		input.z = -previousZDirection
+
+func tryUpdatePreviousDirections():
+	if input.x != 0 and not jumpPreviousX:
+		previousXDirection = input.x
+
+	if input.z != 0 and not jumpPreviousZ:
+		previousZDirection = input.z
+
+func buildTargetPosition(delta : float) -> Vector3:
 	
-	if input.x != 0 and input.z != 0:
-		input.z = -input.z
+	var angle = -(input * delta * angularSpeed)
+	angle += rotation
 	
-	rotation = input * maxAngle
+	var targetX = Math.MinMax(angle.x, -maxAngle, maxAngle)
+	var targetZ = Math.MinMax(angle.z, -maxAngle, maxAngle)
+	
+	if jumpPreviousX and ((previousXDirection > 0 and targetX > 0) or (previousXDirection < 0 and targetX < 0)):
+		targetX = 0
+		
+	if jumpPreviousZ and ((previousZDirection > 0 and targetZ > 0) or (previousZDirection < 0 and targetZ < 0)):
+		targetZ = 0
+	
+	return Vector3(targetX, 0, targetZ)
